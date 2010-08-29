d932e794b5483d9efdd6156611779b7fe1449640
Simone Carletti <weppos@weppos.net>
2010-08-29 19:56:03 +0200

    Add support for caching via Squeezon.cache

--- lib/squeezon.rb
+++ lib/squeezon.rb
@@ -2,4 +2,45 @@ require "boot"
 require "squeezon/feed"
 
 module Squeezon
+  extend self
+
+  # Gets the @cache_store value.
+  #
+  # Returns a ActiveSupport::Cache::Store if a store has been set,
+  # nil otherwise.
+  def cache_store
+    @cache_store
+  end
+
+  # Sets the @cache_store to given store.
+  #
+  # store - The Symbol :store to set, along with options if any.
+  #
+  # Returns a ActiveSupport::Cache::Store if a store has been set,
+  # nil otherwise.
+  def cache_store=(store)
+    @cache_store = ActiveSupport::Cache.lookup_store(store)
+  end
+
+  # Lazy-initializes and returns the current cache handler.
+  # If no store is set, defaults the current store to :file_store.
+  #
+  # Returns a ActiveSupport::Cache::Store.
+  def cache
+    if self.cache_store
+      self.cache_store
+    else
+      self.cache_store = :file_store, root.join("tmp").to_s
+      self.cache_store
+    end
+  end
+
+  # Gets a Pathname instance pointing to the root folder
+  # of this library (not /lib).
+  #
+  # Returns a Pathname.
+  def root
+    @root ||= Pathname.new(File.expand_path("../../", __FILE__))
+  end
+
 end
--- spec/squeezon/squeezon_spec.rb
+++ spec/squeezon/squeezon_spec.rb
@@ -1,4 +1,34 @@
 require "spec_helper"
 
 describe Squeezon do
+
+  describe ".root" do
+    it "should return a Pathname" do
+      Squeezon.root.should be_instance_of(Pathname)
+    end
+
+    it "should return the expanded path to the library root" do
+      Squeezon.root.to_s.should == File.expand_path("../../../", __FILE__)
+    end
+  end
+
+
+  describe ".cache" do
+    it "returns the cache handler" do
+      Squeezon.cache.should be_kind_of(ActiveSupport::Cache::Store)
+
+      store = ActiveSupport::Cache::MemoryStore.new
+      Squeezon.instance_variable_set("@cache_store", store)
+      Squeezon.cache.should be_equal(store)
+
+      store = ActiveSupport::Cache::FileStore.new("/")
+      Squeezon.instance_variable_set("@cache_store", store)
+      Squeezon.cache.should be_equal(store)
+    end
+
+    it "defaults the cache handler to :file_store" do
+      Squeezon.cache.should be_instance_of(ActiveSupport::Cache::FileStore)
+    end
+  end
+
 end
