0f5a60c02d2f40930f15a1525e1226b4ee4a7d45
Simone Carletti <weppos@weppos.net>
2013-12-07 00:15:27 +0100

    Prepare for Heroku

--- lib/squeezon/app.rb
+++ lib/squeezon/app.rb
@@ -34,12 +34,11 @@ module Squeezon
     end
 
     get "/api/feed/entries/*" do
-      GC.start
       feed = Squeezon::Feed.new(params[:splat].first)
-      data = feed.entries.to_squeezon
+      # data = feed.entries.to_squeezon
 
-      json data, callback: params[:callback]
-      # json({ entries: [] }, callback: params[:callback])
+      # json data, callback: params[:callback]
+      json({ entries: [] }, callback: params[:callback])
     end
 
 
